eda7ad9bbd6cb49a2fda2288982a15432be8b275
bp singh <yuvibana03@gmail.com>
2025-12-01 11:06:15 +0530

    add more file/folder added

--- machine-coding-prep/file-explorer/src/App.jsx
+++ machine-coding-prep/file-explorer/src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useCallback } from "react";
 import data from "./data.json";
 
-const FolderIcon = ({ open }) => <span>{open ? "-" : "+"}</span>;
+const FolderIcon = ({ open, toggle, nodeName }) => <span onClick={() => toggle(nodeName)}>{open ? "-" : "+"}</span>;
 
-const Node = ({ NodeList, expanded, toggle }) => {
+const Node = ({ NodeList, expanded, toggle, handleAddFolder }) => {
 
   return (
     <div className="container">
@@ -12,16 +12,22 @@ const Node = ({ NodeList, expanded, toggle }) => {
           key={i}
         >
           <div
-            onClick={() => toggle(node.name)}
             style={{ display: "flex", gap: "10px", cursor: "pointer" }}
           >
-            {node.isfolder && <FolderIcon open={expanded[node.name]} />}
+            {node.isFolder && <FolderIcon open={expanded[node.name]} toggle={toggle} nodeName={node.name} />}
             <span className="listname">{node.name}</span>
+            {
+              node.isFolder && <button
+                onClick={() => handleAddFolder(node.id)}
+              >
+                <img width={20} src="https://cdn-icons-png.flaticon.com/128/11366/11366824.png" alt="" />
+              </button>
+            }
           </div>
 
           {expanded[node.name] && node.children && (
             <div className="item-card" style={{ marginLeft: 20 }}>
-              <Node NodeList={node.children} expanded={expanded} toggle={toggle} />
+              <Node NodeList={node.children} expanded={expanded} toggle={toggle} handleAddFolder={handleAddFolder} />
             </div>
           )}
         </div>
@@ -31,14 +37,40 @@ const Node = ({ NodeList, expanded, toggle }) => {
 };
 
 export default function App() {
+  const [folderData, setFolderData] = useState(data)
   const [expanded, setExpanded] = useState({});
+
+  const handleAddFolder = (parentId) => {
+    const name = prompt();
+    if (name?.trim() == "") return;
+    const updatedTree = (list) => {
+      console.log(list);
+
+      return list?.map((node) => {
+        if (node.id === parentId) {
+          return {
+            ...node,
+            children: [
+              ...node.children,
+              { id: Math.random(), name: name, isFolder: true, children: [] }
+            ]
+          }
+        }
+        if (node.children) {
+          return { ...node, children: updatedTree(node.children) }
+        }
+        return node;
+      })
+    }
+    setFolderData((prev) => updatedTree(prev))
+  }
 
   const toggle = useCallback((key) => {
     setExpanded(prev => ({ ...prev, [key]: !prev[key] }));
   }, []);
   return (
     <div>
-      <Node NodeList={data} expanded={expanded} toggle={toggle} />
+      <Node NodeList={folderData} expanded={expanded} toggle={toggle} handleAddFolder={handleAddFolder} />
     </div>
   );
 }
